9167df11c7d2dcdc89ed602a416365cf1d75c830
Judah Perez <me@judahperez.com>
2025-01-26 18:33:15 -0800

    Setting up sensor for checking colliders

--- src/spawning/mod.rs
+++ src/spawning/mod.rs
@@ -2,6 +2,12 @@ use avian3d::prelude::*;
 use bevy::prelude::*;
 use std::f32::consts::PI;
 
+#[derive(Component)]
+pub struct MilkSpot;
+
+#[derive(Component)]
+pub struct HomeSpot;
+
 pub fn load_world(mut commands: Commands, server: Res<AssetServer>) {
     commands.spawn((
         Transform::from_xyz(0.0, 3.0, 10.0).looking_at(Vec3::ZERO, Vec3::Y),
@@ -14,12 +20,51 @@ pub fn load_world(mut commands: Commands, server: Res<AssetServer>) {
     ));
 
     commands.spawn((
-        Transform::from_translation(Vec3{x: 0.0, y: -0.5, z: 0.0}),
+        Transform::from_translation(Vec3 {
+            x: 0.0,
+            y: -0.5,
+            z: 0.0,
+        }),
         RigidBody::Static,
-        Collider::cuboid(4000.0, 1.0, 4000.0)
+        Collider::cuboid(4000.0, 1.0, 4000.0),
     ));
 
-    load_truck(&mut commands, &server, Vec3 { x: 10.0, y: 0.0, z: 10.0}, PI * 0.5);
+    commands.spawn((
+        Transform::from_translation(Vec3 {
+            x: 50.0,
+            y: 0.0,
+            z: 100.0,
+        }),
+        RigidBody::Static,
+        Collider::sphere(10.0),
+        Sensor,
+        MilkSpot,
+        Name::new("Milk Spot"),
+    ));
+
+    commands.spawn((
+        Transform::from_translation(Vec3 {
+            x: 0.0,
+            y: 0.0,
+            z: 0.0,
+        }),
+        RigidBody::Static,
+        Collider::sphere(10.0),
+        Sensor,
+        HomeSpot,
+        Name::new("Home Spot"),
+    ));
+
+    load_truck(
+        &mut commands,
+        &server,
+        Vec3 {
+            x: 10.0,
+            y: 0.0,
+            z: 10.0,
+        },
+        PI * 0.5,
+    );
     load_person(
         &mut commands,
         &server,
@@ -99,17 +144,23 @@ fn load_house(
 ) {
     // spawn a whole scene
     let my_scene: Handle<Scene> = server.load("models/placeholder/house.glb#Scene0");
-    commands.spawn((
-        Transform::from_translation(translation).with_rotation(Quat::from_rotation_y(rotation)),
-        SceneRoot(my_scene),
-    )).with_children(|parent| {
-        // child cube
-        parent.spawn((
-            Transform::from_translation(Vec3{x: 0.0, y: 5.0, z: 0.0}),
-            RigidBody::Static,
-            Collider::cuboid(10.0, 10.0, 10.0)
-        ));
-    });
+    commands
+        .spawn((
+            Transform::from_translation(translation).with_rotation(Quat::from_rotation_y(rotation)),
+            SceneRoot(my_scene),
+        ))
+        .with_children(|parent| {
+            // child cube
+            parent.spawn((
+                Transform::from_translation(Vec3 {
+                    x: 0.0,
+                    y: 5.0,
+                    z: 0.0,
+                }),
+                RigidBody::Static,
+                Collider::cuboid(10.0, 10.0, 10.0),
+            ));
+        });
 }
 
 fn load_truck(
@@ -120,17 +171,23 @@ fn load_truck(
 ) {
     // spawn a whole scene
     let my_scene: Handle<Scene> = server.load("models/placeholder/truck.glb#Scene0");
-    commands.spawn((
-        Transform::from_translation(translation).with_rotation(Quat::from_rotation_y(rotation)),
-        SceneRoot(my_scene),
-        RigidBody::Dynamic,
-    )).with_children(|parent| {
-        // child cube
-        parent.spawn((
-            Transform::from_translation(Vec3{x: 0.0, y: 2.0, z: 0.0}),
-            Collider::cuboid(6.0, 4.0, 10.0)
-        ));
-    });
+    commands
+        .spawn((
+            Transform::from_translation(translation).with_rotation(Quat::from_rotation_y(rotation)),
+            SceneRoot(my_scene),
+            RigidBody::Dynamic,
+        ))
+        .with_children(|parent| {
+            // child cube
+            parent.spawn((
+                Transform::from_translation(Vec3 {
+                    x: 0.0,
+                    y: 2.0,
+                    z: 0.0,
+                }),
+                Collider::cuboid(6.0, 4.0, 10.0),
+            ));
+        });
 }
 
 fn load_plane_grass(commands: &mut Commands, server: &Res<AssetServer>, translation: Vec3) {
--- src/trucks/mod.rs
+++ src/trucks/mod.rs
@@ -7,7 +7,7 @@ pub struct TruckPlugin;
 
 impl Plugin for TruckPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Update, update_camera_target);
+        app.add_systems(Update, (update_camera_target, print_collisions, print_started_collisions));
     }
 }
 
@@ -26,6 +26,7 @@ pub fn spawn_player(
     let my_scene: Handle<Scene> = server.load("models/placeholder/truck.glb#Scene0");
     commands
         .spawn((
+            Name::new("Player Truck"),
             Transform::from_translation(translation).with_rotation(Quat::from_rotation_y(rotation)),
             SceneRoot(my_scene),
             Player {},
@@ -47,7 +48,6 @@ pub fn spawn_player(
 }
 
 pub fn update_camera_target(
-    time: Res<Time>,
     players: Query<&Transform, With<Player>>,
     mut pan_orbits: Query<&mut PanOrbitState>,
 ) {
@@ -59,11 +59,16 @@ pub fn update_camera_target(
     let player_transform = single_player.unwrap();
 
     for mut pan_orbit in &mut pan_orbits {
-        pan_orbit.center = player_transform.translation + Vec3{x: 0.0, y: 2.0, z: 0.0};
+        pan_orbit.center = player_transform.translation
+            + Vec3 {
+                x: 0.0,
+                y: 2.0,
+                z: 0.0,
+            };
 
-        let half_life = half_life_from_precision(1.0, 0.1);
         let player_rotation = player_transform.rotation.to_axis_angle();
         let axis_angle = player_rotation.0.y * player_rotation.1;
+        //let half_life = half_life_from_precision(1.0, 0.1);
         //let lerp = lerp_smooth(
         //    pan_orbit.yaw,
         //    axis_angle,
@@ -95,4 +100,29 @@ pub fn lerp_smooth(a: f32, b: f32, dt: f32, half_life: f32) -> f32 {
 pub fn half_life_from_precision(duration: f32, precision: f32) -> f32 {
     let half_life = -duration / f32::log2(precision);
     half_life
+}
+
+fn print_collisions(mut collision_event_reader: EventReader<Collision>) {
+    for Collision(contacts) in collision_event_reader.read() {
+        if !contacts.is_sensor {
+            return;
+        }
+
+        // TODO: Get when truck enters trigger / sensor
+
+        info!(
+            "Entities {} and {} are colliding",
+            contacts.entity1, contacts.entity2,
+        );
+    }
+}
+
+fn print_started_collisions(mut collision_event_reader: EventReader<CollisionStarted>) {
+    for CollisionStarted(entity1, entity2) in collision_event_reader.read() {
+        debug!(
+            "Entities {} and {} started colliding",
+            entity1,
+            entity2,
+        );
+    }
 }
